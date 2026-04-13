e9f1fc129f6cb44d7ac460e8faf48be43c68aa11
41891-ux <41891@raphaeldisanto.com.br>
2026-04-13 09:41:00 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+    <main>
+      <section>
+    
+      </section>
+      <div className='bloco1'>
+        <div className='bloco2'></div>
+        </div>
+    
+    </main>
+    </>
+  )
+}
